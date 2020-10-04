4efa9b8ef029e1f4869e1ed01a687ebe4e47c56b
luchompi <lpichon63@gmail.com>
2020-10-03 20:28:45 -0500

    tratando de hacer template

--- PAF/urls.py
+++ PAF/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name="autor"
 
 urlpatterns = [
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
     #path('nuevo/', views.autor_view(), name='autor_crear'),
 ]
